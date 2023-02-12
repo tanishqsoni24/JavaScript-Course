e182d8fd371a282ab530d3fd5a885cd6d8e128d8
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-02-12 17:18:40 +0530

    mod: Variables_Constants

--- Variables_Constants.js
+++ Variables_Constants.js
@@ -1,6 +1,14 @@
 var a = 10;
 a="Tanishq" // Dynamically Typed Language
-// var a = "Soni" - can defined again
+{
+    var a = 50;
+}
+// The global value of a will be now 50 whether it is defined only in the block.
 let b = 125;
+{
+    let b = "Tanishq";
+}
+// The global value of a will be now 125 whether it is redefined in the block.
+// let a = 122;
 // let b = "Tanishq" - can't defined again
 const c = "Tanishq" // Immutable String
