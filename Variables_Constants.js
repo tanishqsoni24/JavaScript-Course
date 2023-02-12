9c72e414c7a14f16a087939ad476d7cdb86324eb
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-02-12 16:33:30 +0530

    variables&constants

---
+++ Variables_Constants.js
@@ -0,0 +1,6 @@
+var a = 10;
+a="Tanishq" // Dynamically Typed Language
+// var a = "Soni" - can defined again
+let b = 125;
+// let b = "Tanishq" - can't defined again
+const c = "Tanishq" // Immutable String
