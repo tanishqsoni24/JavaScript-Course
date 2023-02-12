428c7c23deb6aa588f4e719d988867dea54d502a
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-02-12 23:20:00 +0530

    Add: String-methods

---
+++ basics/strings/String_Methods.js
@@ -0,0 +1,10 @@
+let a = "              Tanishq Soni                     ";
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.slice(2,4));
+console.log(a.slice(2));
+console.log(a.replace("Soni","SONI"));
+console.log(a.includes("SONI"));
+console.log(a.includes("Soni"));
+console.log(a.trim())
