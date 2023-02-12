e15858a30318498d45221201a17da9bad03259bf
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-02-12 18:13:51 +0530

    Add: Primitives_and_Object

---
+++ Primitives_Objects.js
@@ -0,0 +1,20 @@
+// nn bb ss u - Number Null Boolean BigInt String Symbol Undefined
+let a = null; 
+let b = 10;
+let c = "Tanishq"; 
+let d = BigInt("243") + BigInt("07");
+let e = undefined; 
+let f = Symbol("This is Symbol") 
+let g = true; 
+console.log(a, b, c, d, e, f);
+
+// Objects
+
+const values = {
+    Values: true,
+    Name: "Tanishq",
+    Section: "D",
+    Univ_Rno: 2115001034
+}
+
+console.log(values.Name);
