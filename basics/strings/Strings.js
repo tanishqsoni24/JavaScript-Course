525f17af2583841f5a5a0ae2b64e430a69fbe50d
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-02-12 23:01:45 +0530

    mod: Renaming

---
+++ basics/strings/Strings.js
@@ -0,0 +1,12 @@
+// Template Literals
+
+let boy1 = "Tanishq Soni"
+let boy2 = "Shubh Chaturvedi"
+let sentance = `${boy1} is best friend of ${boy2}`
+console.log(sentance);
+
+// Escape Sequences
+
+console.log("Tanishq\"Soni");
+console.log("Tanishq\nSoni");
+console.log("Tanishq\rSoni");
